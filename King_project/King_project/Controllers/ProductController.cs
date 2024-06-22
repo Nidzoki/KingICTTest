c71734381b8f7bd420f45f94e678e11b843309ee
Nidzoki <nikolavidovic01@gmail.com>
2024-06-22 16:58:25 +0200

    Initial commit, working getAllProducts endpoint

---
+++ King_project/King_project/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using King_project.Entites;
+using Microsoft.Net.Http.Headers;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace King_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        static readonly HttpClient client = new();
+        readonly JsonSerializerOptions options = new() { PropertyNameCaseInsensitive = true };
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            Uri uri = new("https://dummyjson.com/products?select=id,title,price,description,images");
+            HttpResponseMessage response = await client.GetAsync(uri).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+
+                
+
+                var products = JsonSerializer.Deserialize<ProductsResponse>(json, options);
+                return Ok(products );
+            }
+
+            return BadRequest(response.ReasonPhrase);
+        }
+    }
+
+    public class ProductsResponse
+    {
+        public required List<Product> Products { get; set; }
+    }
+}
---
+++ King_project/King_project/Entites/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace King_project.Entites
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }   
+        public string Description { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public string[]? Images { get; set; }
+
+    }
+}
